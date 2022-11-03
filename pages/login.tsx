3202f243866052edfd781b3e169310d9f5f2adff
Eriks Muhins <chad@eriksmuhins.com>
2022-11-03 17:34:00 +0200

    Add google auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,25 @@
 import { supabase } from "../services/supabaseClient";
 import { useState } from "react";
+import styles from "../styles/Login.module.css";
 
 export default function Login() {
+  const signInWithGoogle = async () => {
+    try {
+      const response = await supabase.auth.signInWithOAuth({
+        provider: "google",
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>This is the login page</h1>
+      <button className={styles.container} onClick={signInWithGoogle}>
+        Sign in with google
+      </button>
     </div>
   );
 }
