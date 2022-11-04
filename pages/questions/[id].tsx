49bb87c55cc1fdd8a17e3ff92a02d0dbd1547baf
Eriks Muhins <chad@eriksmuhins.com>
2022-11-04 08:14:43 +0200

    Server side render question pages

--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -5,6 +5,7 @@ import html from "remark-html";
 import { supabase } from "../../services/supabaseClient";
 import sanitizeHtml from "sanitize-html";
 
+/*
 // This function receives the `params` that are passed to
 // the page. Here it receives an `id` and uses it to
 // determine the qestion to display
@@ -34,17 +35,42 @@ export async function getStaticProps({ params }) {
 // of the path. Since the name of the path is it's `id` we
 // fetch all the id's from the `feed` table
 export async function getStaticPaths() {
-  /*
   const { data, error, status } = await supabase
     .from("feed")
     .select("id")
     .order("id", { ascending: true });
-     */
-  const paths = [];
-  for (let i = 0; i < 10000; ++i) paths.push({ params: { id: i.toString() } });
+  const paths = data.map((item) => {
+    return {
+      params: {
+        id: item.id.toString(),
+      },
+    };
+  });
   return {
     paths,
     fallback: false,
+  };
+}*/
+
+export async function getServerSideProps({ params }) {
+  const { data, error, status } = await supabase
+    .from("feed")
+    .select("*")
+    .order("id", { ascending: true });
+  const questionData = data[params.id - 1];
+
+  // Here we also sneek in markdown proccesing since we need
+  // to do it on server side (this function runs server-side
+  // as opposed to client-side)
+  const processedContent = await remark()
+    .use(html)
+    .process(questionData.content);
+  const contentHtml = sanitizeHtml(processedContent.toString());
+  return {
+    props: {
+      questionData,
+      contentHtml,
+    },
   };
 }
 
